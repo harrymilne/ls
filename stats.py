19ec6faf6a11069a3f8d7eacd9b3bd94a9191864
Harry Milne <harry.milne@me.com>
2013-09-19 22:19:02 +0100

    added individual server stats

--- stats.py
+++ stats.py
@@ -5,7 +5,7 @@ import pickle
 class Stats:
 	def __init__(self, prefs):
 		self.file_name = prefs["stats"]
-		self.stats = {"active_hours":{}}
+		self.stats = {"active_hours":{}, "active_servers":{}}
 		if os.path.exists(self.file_name):
 			self.stats = self.load()
 
@@ -19,16 +19,32 @@ class Stats:
 		
 	def log(self, server_data):
 		hour = time.strftime("%H", time.gmtime())
-		if hour in self.stats["active_hours"]:
-			self.stats["active_hours"][hour] += self.get_players_online(server_data)
-		else:
-			self.stats["active_hours"][hour] = self.get_players_online(server_data)
+		self.set_players_online(server_data, hour)
+		self.set_server_players(server_data, hour)
 		self.save()
 
+	#WIP to record how long the data has been recorded for
+	def new_day(self):
+		if "date" not in self.stats:
+			self.stats["date"] = time.strftime("%d/%m/%Y", time.gmtime())
 
-	def get_players_online(self, server_data):
+	def set_players_online(self, server_data, hour):
 		players_online = 0
 		for server in server_data:
 			players_online += len(server_data[server]["players"])
-		return players_online
+
+		if hour in self.stats["active_hours"]:
+			self.stats["active_hours"][hour] += players_online
+		else:
+			self.stats["active_hours"][hour] = players_online
+
+	def set_server_players(self, server_data, hour):
+		for server in server_data:
+			players = len(server_data[server]["players"])
+			if server not in self.stats["active_servers"]:
+				self.stats["active_servers"][server] = {}
+			if hour in self.stats["active_servers"]:
+				self.stats["active_servers"][server][hour] += players
+			else:
+				self.stats["active_servers"][server][hour] = players
 
