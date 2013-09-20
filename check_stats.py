37be58c4e3808ca880f0f032592ca01f35f883fd
Harry Milne <harry.milne@me.com>
2013-09-20 22:09:39 +0100

    more robust stat implementation
    each day is stored as a object in an array, array will retain order and
    each object has self.date_recorded

--- check_stats.py
+++ check_stats.py
@@ -2,11 +2,21 @@ from prefs import Prefs
 from pprint import pprint
 import pickle
 
+class Activity:
+	def __init__(self, date):
+		self.date_recorded = date
+		self.hours = {}
+		self.servers = {}
+
 def get_stats(prefs):
 	with open(prefs["stats"], "rb") as stat_file:
 		return pickle.load(stat_file)
 
 if __name__ == "__main__":
 	prefs = Prefs("prefs.ini").open_prefs()
-	pprint(get_stats(prefs))
+	stats_list = get_stats(prefs)
+	for i in stats_list:
+		print(i.date_recorded)
+		pprint(i.hours)
+		pprint(i.servers)
 
--- stats.py
+++ stats.py
@@ -2,12 +2,19 @@ import time
 import os
 import pickle
 
+class Activity:
+	def __init__(self, date):
+		self.date_recorded = date
+		self.hours = {}
+		self.servers = {}
+
 class Stats:
 	def __init__(self, prefs):
-		self.file_name = prefs["stats"]
-		self.stats = {"active_hours":{}, "active_servers":{}}
+		self.file_name 	= prefs["stats"]
+		self.stats 		= []
+		self.date 		= "0/0/0"
 		if os.path.exists(self.file_name):
-			self.stats = self.load()
+			self.stats 	= self.load()
 
 	def load(self):
 		with open(self.file_name, "rb") as stats_file:
@@ -19,32 +26,37 @@ class Stats:
 		
 	def log(self, server_data):
 		hour = time.strftime("%H", time.gmtime())
+		if self.new_day():
+			new_date = time.strftime("%d/%m/%Y", time.gmtime())
+			self.stats.append(Activity(new_date))
+			self.date = new_date
 		self.set_players_online(server_data, hour)
 		self.set_server_players(server_data, hour)
 		self.save()
 
-	#WIP to record how long the data has been recorded for
 	def new_day(self):
-		if "date" not in self.stats:
-			self.stats["date"] = time.strftime("%d/%m/%Y", time.gmtime())
+		if self.date != time.strftime("%d/%m/%Y", time.gmtime()):
+			return True
+		else:
+			return False
 
 	def set_players_online(self, server_data, hour):
 		players_online = 0
 		for server in server_data:
 			players_online += len(server_data[server]["players"])
 
-		if hour in self.stats["active_hours"]:
-			self.stats["active_hours"][hour] += players_online
+		if hour in self.stats[-1].hours:
+			self.stats[-1].hours[hour] += round(players_online/60)
 		else:
-			self.stats["active_hours"][hour] = players_online
+			self.stats[-1].hours[hour] = round(players_online/60)
 
 	def set_server_players(self, server_data, hour):
 		for server in server_data:
 			players = len(server_data[server]["players"])
-			if server not in self.stats["active_servers"]:
-				self.stats["active_servers"][server] = {}
-			if hour in self.stats["active_servers"]:
-				self.stats["active_servers"][server][hour] += players
+			if server not in self.stats[-1].servers:
+				self.stats[-1].servers[server] = {}
+			if hour in self.stats[-1].servers[server]:
+				self.stats[-1].servers[server][hour] += round(players/60)
 			else:
-				self.stats["active_servers"][server][hour] = players
+				self.stats[-1].servers[server][hour] = round(players/60)
 
