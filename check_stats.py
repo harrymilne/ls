0bfcee97e57ecda5d5e14af924840d145df0ca94
Harry Milne <harry.milne@me.com>
2013-09-22 21:21:43 +0100

    fixed to work with new stats

--- check_stats.py
+++ check_stats.py
@@ -1,6 +1,7 @@
 from prefs import Prefs
 from pprint import pprint
 import pickle
+import os
 
 class Activity:
 	def __init__(self, date):
@@ -9,14 +10,20 @@ class Activity:
 		self.servers = {}
 
 def get_stats(prefs):
-	with open(prefs["stats"], "rb") as stat_file:
-		return pickle.load(stat_file)
+	stat_file = prefs["stats"]
+	if os.path.exists(stat_file):
+		with open(stat_file, "rb") as stat_file:
+			return pickle.load(stat_file)
+	else:
+		return "prefs.ini does not exist."
 
 if __name__ == "__main__":
 	prefs = Prefs("prefs.ini").open_prefs()
 	stats_list = get_stats(prefs)
-	for i in stats_list:
-		print(i.date_recorded)
-		pprint(i.hours)
-		pprint(i.servers)
+	print(stats_list)
+	if type(stats_list) == type([]):
+		for i in stats_list:
+			print(i.date_recorded)
+			pprint(i.hours)
+			pprint(i.servers)
 
