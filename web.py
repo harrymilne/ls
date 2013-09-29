dcda26fec9326866c7bb5f3f2a1bbdb3e9e07730
Harry Milne <harry.milne@me.com>
2013-09-29 15:41:33 +0100

    added graphs
    *fixed socket misformatting

--- web.py
+++ web.py
@@ -1,5 +1,9 @@
 import time
+import os
+import pickle
 import jinja2 as jin
+from stats import Total
+
 
 class Webpage:
 
@@ -7,10 +11,12 @@ class Webpage:
 		self.env 	= jin.Environment(loader=jin.FileSystemLoader("templates/"))
 		self.index 	= self.env.get_template("index.html")
 		self.js 	= self.env.get_template("main.js")
+		self.graph 	= self.env.get_template("graph.html")
 		self.prefs 	= prefs
 
 	def write(self, server_data):
 		self.write_HTML(server_data)
+		self.write_graph()
 
 	def write_HTML(self, server_data):
 		servers_online = sorted([server for server in server_data if bool(len(server_data[server]["players"]))])
@@ -36,5 +42,21 @@ class Webpage:
 		with open(self.prefs["js"], mode="w", encoding="utf-8") as js_f:
 			js_f.write(rendered)
 		print("MSG: JS Written.")
+
+	def write_graph(self):
+		if os.path.exists("total.bin"):
+			with open("total.bin", "rb") as total_f:
+				total = pickle.load(total_f)
+			graph_name 	= "Total Activity Data"
+			hour_list 	= sorted(list(total.hours.keys()))
+			xy_data 	= zip(hour_list, [total.hours[i] for i in hour_list])
+			caption 	= "Days sampled: {0}".format(", ".join(total.records_included))
+			rendered = self.graph.render(
+				title			= graph_name, 
+				player_activity	= xy_data,
+				caption 		= caption)
+			file_name = graph_name.lower().replace(" ","-")+".html"
+			with open(self.prefs["graphs"]+file_name, mode="w", encoding="utf-8") as graph_f:
+				graph_f.write(rendered)
 
 
